293440094231417d307cf39a586d2a27382d5d50
Rabi León <rabi_leon@hotmail.com>
2021-11-13 13:21:58 -0600

    🚧 middlewares

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const routerApi = require('./routes');
+
+const { logErrors, errorHandler } = require('./middlewares/error.handler');
+
 const app = express();
 const port = 3000;
 
@@ -18,6 +21,9 @@ app.get('/categories/:categoryId/products/:productId', (req, res) => {
 });
 
 routerApi(app);
+
+app.use(logErrors);
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log('my port' + port);
